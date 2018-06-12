ffb1713cdda7444ac57ccaffd9ed8d6f51903492
Matheus Gobbi <mgobbi.10@gmail.com>
2018-06-11 21:00:08 -0300

    teste2

--- TrabalhoFinal/TelaPedido.Designer.cs
+++ TrabalhoFinal/TelaPedido.Designer.cs
@@ -46,7 +46,6 @@
             this.txtLogradouro = new System.Windows.Forms.TextBox();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.bttAdcLanche = new System.Windows.Forms.Button();
@@ -223,13 +222,6 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(0, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 20;
-            // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.Silver;
@@ -295,7 +287,6 @@
             this.Controls.Add(this.bttAdcLanche);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
@@ -329,7 +320,6 @@
         private System.Windows.Forms.TextBox txtLogradouro;
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button bttAdcLanche;
